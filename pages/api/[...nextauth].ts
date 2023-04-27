66f811f5b50eb7d8a8b1ba63bae0ac221d31f0b4
gabriel silva <gabriel.gs605@gmail.com>
2023-04-27 20:12:36 -0300

    first commit

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,49 @@
+import { AuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import prisma from '@/app/libs/prismadb';
+import CredentialProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'emaill', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid credentials');
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid credentials');
+        }
+        const isCorrectPasswword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPasswword) {
+          throw new Error('Invalid credentials');
+        }
+        return user;
+      },
+    }),
+  ],
+};
